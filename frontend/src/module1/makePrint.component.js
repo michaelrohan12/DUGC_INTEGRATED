1eba7245417590a6fbfadfb60d5063f707371415
michaelrscodes@gmail.com <michaelrscodes@gmail.com>
2022-12-15 12:57:53 +0530

    Module 1 has been updated

--- frontend/src/module1/makePrint.component.js
+++ frontend/src/module1/makePrint.component.js
@@ -1,66 +1,91 @@
-import React, { Component, Fragment } from "react";
 import axios from "axios";
+import React, { Component, Fragment } from "react";
 import ReactToPrint from "react-to-print";
 
-export default class MakePrint extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            student: [],
-            sem: 0,
-        }
-    }
+export default class MakePrint extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      student: [],
+      sem: 0,
+    };
+  }
 
-    onChangeSem = (e) =>{
-        this.setState({sem: e.target.value});
+  onChangeSem = (e) => {
+    this.setState({ sem: e.target.value });
 
-        axios.get(`http://localhost:1999/api/makeUpMinor/apprMakeUp?&year=${(new Date()).getFullYear()}&sem=${(e.target.value)}`)
-            .then(Response => {
-                console.log(Response.data.data)
-                this.setState({ student: Response.data.data })
-                // console.log(students);
-            })
-            .catch(error => {
-                console.log("Error" + error);
-            })
-    }
+    axios
+      .get(
+        `http://localhost:1999/api/makeUpMinor/apprMakeUp?&year=${new Date().getFullYear()}&sem=${
+          e.target.value
+        }`
+      )
+      .then((Response) => {
+        console.log(Response.data.data);
+        this.setState({ student: Response.data.data });
+        // console.log(students);
+      })
+      .catch((error) => {
+        console.log("Error" + error);
+      });
+  };
 
-    render(){
-        return(
-            <div>
-                <label>Sem</label>
-                <input type={"number"} value={this.state.sem} onChange={this.onChangeSem} min={3} max={8}></input>
-                <div ref={el=>(this.componentRef = el)}>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Student Name</th>
-                                <th>Course</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.student.map((stud,index)=>{
-                                return(<Fragment key={index}>
-                                    {stud.courses.map((cour) => {
-                                    return (
-                                        <tr key={cour._id}>
-                                            <td>{stud.studentName}</td>
-                                            <td>{cour.course}</td>
-                                        </tr>
-                                    )
-                                })}
-                                </Fragment>);
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-                <ReactToPrint trigger={() => {
-                return <button className="btn btn-secondary"  style={{marginLeft:'50%'}} >Print</button>
-                }}
-                content={() => this.componentRef}
-                pageStyle="print"
-            />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <label>Sem</label>
+        <input
+          type={"number"}
+          value={this.state.sem}
+          onChange={this.onChangeSem}
+          min={3}
+          max={8}
+        ></input>
+        <div ref={(el) => (this.componentRef = el)}>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Student Name</th>
+                <th>Course</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.student.map((stud, index) => {
+                return (
+                  <Fragment key={index}>
+                    {stud.courses.map((cour) => {
+                      if (cour.approval === true) {
+                        return (
+                          <tr key={cour._id}>
+                            <td>{stud.studentName}</td>
+                            <td>{cour.course}</td>
+                          </tr>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </Fragment>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <button
+                className="btn btn-secondary"
+                style={{ marginLeft: "50%" }}
+              >
+                Print
+              </button>
+            );
+          }}
+          content={() => this.componentRef}
+          pageStyle="print"
+        />
+      </div>
+    );
+  }
 }
